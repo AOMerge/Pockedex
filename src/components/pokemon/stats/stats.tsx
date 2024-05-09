1f7677ecbc9245214aba18c8557cf13cde5412c0
Ángel Ortega <codenode.ortega@gmail.com>
2024-05-09 03:23:30 -0600

    Refactor and update file extensions in stats.tsx

--- src/components/pokemon/stats/stats.tsx
+++ src/components/pokemon/stats/stats.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { View, Text, SafeAreaView, TextInput } from "react-native";
 import { PockemonStatsStyle as Style } from "../../../styles/styles";
 
-export function Stats({ stats }) {
+export function Stats({ stats }: { stats: any }) {
     
   return (
     <View style={Style.container}>
       <View>
         <Text style={Style.title}>Stats</Text>
         <View>
-          {stats.map((item, index) => (
+          {stats.map((item: any, index: number) => (
             <View style={{ flexDirection: "row" }} key={index}>
               <View
                 style={[Style.bodyContainer, { width: `${item.base_stat}%` }]}
